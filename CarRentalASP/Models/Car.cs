c383c429b9f958b0cdc9972cd02e5969902aa027
The_Scriptures <elbetelworkineh59@gmail.com>
2020-01-19 19:41:56 -0800

    Oracle in .NET

---
+++ CarRentalASP/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarRentalASP.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string CarBrand { get; set; }
+        public string CarModel { get; set; }
+        public String Color { get; set; }
+        public DateTime ManufacturedDate { get; set; }
+        public double RentalAmount { get; set; }
+    }
+}
